956a4ebb38062fe89ecb510abe278177ec66758f
Philip Bauer <bauer@starzel.de>
2018-03-19 17:14:06 +0100

    fix startup in py3

--- src/z3c/caching/registry.py
+++ src/z3c/caching/registry.py
@@ -10,7 +10,7 @@ the ruleset id.
 
 import warnings
 
-from zope.interface import implements, Interface, Attribute
+from zope.interface import implementer, Interface, Attribute
 
 from zope.component import adapts, queryUtility, getUtilitiesFor, getGlobalSiteManager
 from zope.component.interfaces import IComponents
@@ -19,57 +19,57 @@ from z3c.caching.interfaces import IRulesetRegistry
 from z3c.caching.interfaces import IRulesetType
 
 class ICacheRule(Interface):
-    """Represents the cache rule applied to an object.    
+    """Represents the cache rule applied to an object.
     This is strictly an implementation detail of IRulesetRegistry.
     """
-    
+
     id = Attribute("The identifier of this cache rule")
 
+@implementer(ICacheRule)
 class CacheRule(object):
     __slots__ = ("id")
-    implements(ICacheRule)
-    
+
     def __init__(self, identifier):
         self.id = identifier
 
+@implementer(IRulesetType)
 class RulesetType(object):
     __slots__ = ('name', 'title', 'description',)
-    implements(IRulesetType)
-    
+
     def __init__(self, name, title, description):
         self.name = name
         self.title = title
         self.description = description
 
 def get_context_to_cacherule_adapter_factory(rule):
-    """Given a cache rule return an adapter factory which expects an object 
+    """Given a cache rule return an adapter factory which expects an object
     but only returns the pre-specified cache rule."""
     def CacheRuleFactory(context):
         return CacheRule(rule)
     CacheRuleFactory.id = rule
     return CacheRuleFactory
 
+@implementer(IRulesetRegistry)
 class RulesetRegistry(object):
 
-    implements(IRulesetRegistry)
     adapts(IComponents)
 
     def __init__(self, registry):
         self.registry = registry
-        
+
     def register(self, obj, rule):
         rule = str(rule) # We only want ascii, tyvm
-        
+
         if self.explicit and queryUtility(IRulesetType, rule) is None:
             raise LookupError("Explicit mode set and ruleset %s not found" % rule)
-        
+
         factory = get_context_to_cacherule_adapter_factory(rule)
         existing = self.directLookup(obj)
         if existing is None:
             # Only register if we haven't got this one already
             self.registry.registerAdapter(factory, provided=ICacheRule, required=(obj,))
         else:
-            warnings.warn("Ignoring attempted to register caching rule %s for %s.  %s is already registered." % (rule, `obj`, existing))
+            warnings.warn("Ignoring attempted to register caching rule %s for %s.  %s is already registered." % (rule, repr(obj), existing))
         return None
 
 
@@ -85,17 +85,17 @@ class RulesetRegistry(object):
                 continue # Not our responsibility
             else:
                 self.registry.unregisterAdapter(factory=rule.factory,
-                                                provided=rule.provided, 
+                                                provided=rule.provided,
                                                 required=rule.required)
-        
+
         for type_ in list(self.registry.registeredUtilities()):
             if type_.provided != IRulesetType:
                 continue # Not our responsibility
             else:
                 self.registry.unregisterUtility(component=type_.component,
-                                                provided=IRulesetType, 
+                                                provided=IRulesetType,
                                                 name=type_.name)
-        
+
         self.explicit = False
         return None
 
@@ -104,25 +104,25 @@ class RulesetRegistry(object):
         if rule is not None:
             return rule.id
         return None
-    
+
     __getitem__ = lookup
-    
+
     def declareType(self, name, title, description):
         type_ = RulesetType(name, title, description)
         self.registry.registerUtility(type_, IRulesetType, name=name)
-    
+
     def enumerateTypes(self):
         for name, type_ in getUtilitiesFor(IRulesetType):
             yield type_
-    
+
     def _get_explicit(self):
         return getattr(self.registry, '_z3c_caching_explicit', False)
     def _set_explicit(self, value):
         setattr(self.registry, '_z3c_caching_explicit', value)
     explicit = property(_get_explicit, _set_explicit)
-    
+
     # Helper methods
-    
+
     def directLookup(self, obj):
         """Find a rule _directly_ assigned to `obj`"""
         for rule in self.registry.registeredAdapters():
